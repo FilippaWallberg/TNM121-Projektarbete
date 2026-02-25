311aab2323b07f56c400c45292eebae8775405d0
Shalom Woldeab <shawo137@student.liu.se>
2026-02-25 09:57:13 +0100

    cgange

--- server1/index.js
+++ server1/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/tnm121-project")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error("Mongo error:", err));
+
+// Test route
 app.get("/api/health", (req, res) => {
   res.json({ ok: true });
 });
